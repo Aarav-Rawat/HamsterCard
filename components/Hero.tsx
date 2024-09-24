8693407b651ea26ac94ad5f1879a7bb4a8ce5590
AaraV <aaravrawat00000@gmail.com>
2024-09-24 19:08:44 +0530

    front img almost done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,20 +18,74 @@ const Hero = () => {
         <p></p>
         <span>Tap on the Game & get your Combos</span>
       </div>
-      <div>
+      <div className='flex items-center justify-center'>
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
             <AccordionTrigger>
-                  <div>
-                    <Image src="/hamster.png" alt="A cute hamster" width={100} height={100}/>
+                  <div className='w-[50vw] relative overflow-hidden h-[40vh]'>
+                    <Image src="/hamster.png" alt="A cute hamster"  className='absolute'  layout='fill' />
                   </div>
             </AccordionTrigger>
-
             <AccordionContent>
+               <div>Your kombo</div>
               
             </AccordionContent>
 
           </AccordionItem>
+          
+          <AccordionItem value="item-2">
+            <AccordionTrigger>
+                  <div className='w-[50vw] relative overflow-hidden h-[40vh]'>
+                    <Image src="/hamster.png" alt="A cute hamster"  className='absolute'  layout='fill' />
+                  </div>
+            </AccordionTrigger>
+            <AccordionContent>
+               <div>Your kombo</div>
+              
+            </AccordionContent>
+
+          </AccordionItem>
+
+
+          <AccordionItem value="item-3">
+            <AccordionTrigger>
+                  <div className='w-[50vw] relative overflow-hidden h-[40vh]'>
+                    <Image src="/hamster.png" alt="A cute hamster"  className='absolute'  layout='fill' />
+                  </div>
+            </AccordionTrigger>
+            <AccordionContent>
+               <div>Your kombo</div>
+              
+            </AccordionContent>
+
+          </AccordionItem>
+
+          <AccordionItem value="item-4">
+            <AccordionTrigger>
+                  <div className='w-[50vw] relative overflow-hidden h-[40vh]'>
+                    <Image src="/hamster.png" alt="A cute hamster"  className='absolute'  layout='fill' />
+                  </div>
+            </AccordionTrigger>
+            <AccordionContent>
+               <div>Your kombo</div>
+              
+            </AccordionContent>
+
+          </AccordionItem>
+
+          <AccordionItem value="item-5">
+            <AccordionTrigger>
+                  <div className='w-[50vw] relative overflow-hidden h-[40vh]'>
+                    <Image src="/hamster.png" alt="A cute hamster"  className='absolute'  layout='fill' />
+                  </div>
+            </AccordionTrigger>
+            <AccordionContent>
+               <div>Your kombo</div>
+              
+            </AccordionContent>
+
+          </AccordionItem>
+
         </Accordion>
 
       </div>
