d9bdbe45891e9525de0ad9a5e1b39f05bb91c6dc
AaraV <aaravrawat00000@gmail.com>
2024-09-24 18:19:01 +0530

    fixed Imgae problem

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,41 @@
+"use client"
 import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import Image from 'next/image'
+
 
 const Hero = () => {
   return (
-    <main>
-        
-    </main>
+    <>
+      <div>
+
+        <h2>Daily Kombos</h2>
+        <p></p>
+        <span>Tap on the Game & get your Combos</span>
+      </div>
+      <div>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+                  <div>
+                    <Image src="/hamster.png" alt="A cute hamster" width={100} height={100}/>
+                  </div>
+            </AccordionTrigger>
+
+            <AccordionContent>
+              
+            </AccordionContent>
+
+          </AccordionItem>
+        </Accordion>
+
+      </div>
+    </>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='h-14 w-full border-b-2 border-white'>
-        Nav
+    <nav className='h-14 w-full border-b-2 border-white flex items-center'>
+        <h2 className='text-start font-semibold text-xl ml-2'>Hamster Combos</h2>
     </nav>
   )
 }
