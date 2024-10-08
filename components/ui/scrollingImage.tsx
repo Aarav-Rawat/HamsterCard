476d0494b9d3c6ef6cc387b8bc5fd124726c21d6
AaraV <aaravrawat00000@gmail.com>
2024-10-08 17:26:33 +0530

    img loop fixing

--- components/ui/scrollingImage.tsx
+++ components/ui/scrollingImage.tsx
@@ -4,29 +4,68 @@ import { motion } from 'framer-motion';
 import { coins } from '@/data';
 
 const ScrollingImage: React.FC = () => {
-    
+
+    const images = [
+        "/hamsterCombatLogo.jpeg",
+        "/gemzLogo.jpeg",
+        "/MuskEmpireLogo.jpeg",
+        "/mafiaDaoLogo.webp",
+        "/pixelVerseLogo.png",
+        "/rockyRabbitLogo.png",
+        "/grandCombat.webp",
+        "/tapCoins.jpeg",
+        "/blumLogo.png",
+        "/tomarketLogo.png"
+    ]
+
+
     return (
-        <div className="relative overflow-hidden sm:w-full h-16 w-full rounded-full flex items-center justify-cente ">
+        <div className="relative overflow-hidden sm:w-full h-16 w-full rounded-full flex items-center justify-center">
             <motion.div
-                className="flex sm:gap-5 gap-2"
+                className="flex sm:gap-5 gap-2 flex-shrink-0"
                 initial={{ x: '0' }}
-                animate={{ x: '-100%' }} 
+                animate={{ x: '-100%' }}
                 transition={{
                     ease: 'linear',
-                    duration: 8, 
-                    repeat: Infinity, 
+                    duration: 20,
+                    repeat: Infinity,
                 }}
-             
+                style={{ whiteSpace: 'nowrap' }} 
+
             >
-                {coins.map((coin, index) => (
-            <motion.img
-                key={index}
-                src={coin.src}
-                alt="Scrolling"
-                className="h-10 sm:h-14 object-cover rounded-full"
-            />
-        ))}
+                {[...images,...images].map((img, index) => (
+                    <motion.img
+                        key={index}
+                        src={img}
+                        alt="Scrolling"
+                        className="h-10 sm:h-14 object-cover rounded-full inline-block"
+                    />
+                ))}
             </motion.div>
+            <motion.div
+                className="ml-2 flex sm:gap-5 gap-2 flex-shrink-0"
+                initial={{ x: '0' }}
+                animate={{ x: '-100%' }}
+                transition={{
+                    ease: 'linear',
+                    duration: 20,
+                    repeat: Infinity,
+                }}
+                style={{ whiteSpace: 'nowrap' }} 
+
+            >
+                {[...images,...images].map((img, index) => (
+                    <motion.img
+                        key={index}
+                        src={img}
+                        alt="Scrolling"
+                        className="h-10 sm:h-14 object-cover rounded-full inline-block"
+                    />
+                ))}
+            </motion.div>
+
+     
+           
         </div>
     );
 };
