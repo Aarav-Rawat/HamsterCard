b1ac092229adfddcd46579165bfd4ecc9a424fd5
AaraV <aaravrawat00000@gmail.com>
2024-10-14 14:17:05 +0530

    add navigation to newz in navbar

--- components/Newz.tsx
+++ components/Newz.tsx
@@ -3,7 +3,7 @@ import { TextGenerateEffect } from './ui/textGenerateEffect'
 
 const Newz = () => {
   return (
-    <div>
+    <div id="newz">
      <TextGenerateEffect words={"New'z"}  className='text-center pb-4 pt-2 text-3xl font-semibold text-[#C36E22]' />
     </div>
   )
