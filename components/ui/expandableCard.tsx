7eca440b3a95e40744e95000795def9e97cbc15d
AaraV <aaravrawat00000@gmail.com>
2024-10-25 21:20:12 +0530

    gap b/w coins

--- components/ui/expandableCard.tsx
+++ components/ui/expandableCard.tsx
@@ -104,7 +104,7 @@ export function ExpandableCardDemo() {
         ) : null}
       </AnimatePresence>
 
-      <ul className="max-w-2xl  flex flex-col w-full gap-5">
+      <ul className="max-w-2xl flex flex-col w-full gap-7">
         {coins.map((coin, index) => (
           <motion.div
             layoutId={`coin-${coin.title}-${id}`}
